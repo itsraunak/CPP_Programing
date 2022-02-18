9c23c1686e942b59f226dca9c171a3a27b3c738b
Raunak Kumar <itisraunak@gmail.com>
2022-02-18 20:10:53 +0530

    Use of Class

---
+++ use_of_class.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+class person
+{
+    char name[30];
+    int age;
+
+public:
+    void getdata(void);
+    void display(void);
+};
+
+void person ::getdata(void)
+{
+
+    cout << "Enter Name: ";
+    cin >> name;
+    cout << "Enter Age ";
+    cin >> age;
+};
+
+void person ::display(void)
+{
+    cout << "\nName:" << name;
+    cout << "\nAge:" << age;
+};
+
+int main()
+{
+    person p;
+
+    p.getdata();
+    p.display();
+}
